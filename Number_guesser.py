ecca5f525170ef85468a145cbd97c51a66bb858c
Tushar Bisht <tb0712@srmist.edu.in>
2025-06-04 22:01:53 +0530

    Create Number_guesser.py

---
+++ Number_guesser.py
@@ -0,0 +1,24 @@
+import random
+
+print("Welcome to number guessing game-guess the random number ranging from 1 to 10!!\n(dont worry hints will be provided)\nGuess!!")
+random_number = random.randint(1,11)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
